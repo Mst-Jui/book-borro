c4e3e591e255d51bc6575f296f92c8e2e89dca25
Mst-Jui <contact.juiakter@gmail.com>
2026-04-30 22:14:30 +0600

    fix: remove localhost fetch

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  plugins: {
-    '@tailwindcss/postcss': {},
-  },
+
   images: {
     remotePatterns: [
       {
