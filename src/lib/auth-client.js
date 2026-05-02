891d96ea8689bfe294d94a4d65f31df432521590
Mst-Jui <contact.juiakter@gmail.com>
2026-05-02 16:34:59 +0600

    navbar

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,9 +1,12 @@
+
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     
     baseURL: "https://book-borro.vercel.app"
     // baseURL: "http://localhost:3000"
+    
 })
+
 
 
 
