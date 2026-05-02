79b2bf48d8b690d684a52678a52d2120222256c4
Mst-Jui <contact.juiakter@gmail.com>
2026-05-02 10:27:59 +0600

    animation

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,8 +1,8 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     
-    // baseURL: "https://book-borro.vercel.app"
-    baseURL: "http://localhost:3000"
+    baseURL: "https://book-borro.vercel.app"
+    // baseURL: "http://localhost:3000"
 })
 
 
